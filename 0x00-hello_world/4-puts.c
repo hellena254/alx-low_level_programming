be2c80a64ce94dc95164a592c4cf6dd752ee457f
hellenah254 <hellenah243@gmail.com>
2023-09-07 12:45:14 +0300

    puts to the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program uses puts to print.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
