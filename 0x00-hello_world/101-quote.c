821a18f6b84bac13e4434e274377ee3813d7b650
hellenah254 <hellenah243@gmail.com>
2023-09-07 18:56:11 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
