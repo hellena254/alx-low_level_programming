1822192ab07566570f4d68dd84e1c194316a9d7c
hellenah254 <hellenah243@gmail.com>
2023-09-07 19:05:48 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-	char words[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, words, 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
