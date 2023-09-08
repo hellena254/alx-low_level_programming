541f0b42a348ea74b36e12193a308ef02031324a
hellenah254 <hellenah243@gmail.com>
2023-09-08 17:35:32 +0300

    print combination of single-digit code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible combinations of
+ * single digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar('0' + digit + ',' + ' ');
+	putchar('\n');
+	return (0);
+}
