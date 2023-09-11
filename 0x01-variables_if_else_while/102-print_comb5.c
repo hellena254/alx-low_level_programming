fb51d629179e7031d3094c26f106cc4f2dd349dd
hellenah254 <hellenah243@gmail.com>
2023-09-11 08:15:58 +0300

    print all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,11 +18,11 @@ int main(void)
 		/* loop to iiterate the sec digit*/
 		for (num2 = 0; num2 <= 99; num2++)
 		{
-			putchar('0' + num1 / 10);
-			putchar('0' + num1 % 10);
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar('0' + num2 / 10);
-			putchar('0' + num2 % 10);
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 != 99 || num2 != 99)
 			{
