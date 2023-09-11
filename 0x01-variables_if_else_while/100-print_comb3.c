54eda7acda6c512f43a866597b69cdc6a2f30b67
hellenah254 <hellenah243@gmail.com>
2023-09-11 15:36:47 +0300

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,18 @@
  *
  * Description: print different combination of two digits
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int digit1, digit2;
 
-	/* Iterate the first digit */
+	/* Iterate the first digit (0 to 8) */
 	for (digit1 = 0; digit1 <= 8; digit1++)
 	{
-		/* iterate through the second digit */
-		for (digit2 = 0; digit2 <= 9; digit2++)
+		/* iterate through the second digit (1 to 9) */
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 		{
 			/* convert the digits to characters */
 			putchar('0' + digit1);
