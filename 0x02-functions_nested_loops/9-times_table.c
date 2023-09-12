48b13d6be382443affc859a314d7891057bd2e90
hellenah254 <hellenah243@gmail.com>
2023-09-13 01:41:31 +0300

    print the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * times_table: prints the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			/* print the tens digit*/
+			putchar((result / 10) + '0');
+			/* print the ones digit*/
+			putchar((result % 10) + '0');
+
+			if (j == 9)
+			{
+				putchar('\n');
+			} else
+			{
+				putchar('.');
+				putchar(' ');
+			}
+		}
+	}
+}
