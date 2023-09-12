82b9f35ed55cd56c5b3c541a03e3b9c3f5394223
hellenah254 <hellenah243@gmail.com>
2023-09-12 14:32:38 +0300

    prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -17,6 +17,14 @@ int _puchar(char c)
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
