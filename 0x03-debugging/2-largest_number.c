bf89465adb0268a43b63a447c458bd52c16485a9
hellenah254 <hellenah243@gmail.com>
2023-09-13 12:56:57 +0300

    Fix the print_remaining_days() function so that the output works correctly for all dates and all leap years

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,10 +15,12 @@ int largest_number(int a, int b, int c)
 	if (a >= b && b >= c)
 	{
 		largest = a;
-	} else if (b >= a && a >= c)
+	}
+	else if (b >= a && a >= c)
 	{
 		largest = b;
-	} else
+	}
+	else
 	{
 		largest = c;
 	}
