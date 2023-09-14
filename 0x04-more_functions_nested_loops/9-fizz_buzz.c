bdf295985a8080a122d1c08e44416535ca68e6b8
hellenah254 <hellenah243@gmail.com>
2023-09-14 15:28:08 +0300

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,17 @@ int main(void)
 
 	for (i = 1; 1 <= 100; i++)
 	{
-	if ((i % 3) == 0)
-		printf("Fizz ");
+	if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+	else if ((i % 3) == 0)
+		printf("Fizz");
 	else if ((i % 5) == 0)
-		printf("Buzz ");
-	else if (i % 3 == 0 && i % 5 == 0)
-		printf("FizzBuzz ");
+		printf("Buzz");
 	else
-		printf("%d ", i);
+		printf("%d", i);
+
+	if (i != 100)
+		printf(" ");
 	}
 
 	printf("\n");
