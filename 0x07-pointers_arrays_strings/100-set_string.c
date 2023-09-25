08a9d932db17c344f3fb795852f934c619a39406
hellenah254 <hellenah243@gmail.com>
2023-09-26 02:43:41 +0300

    print sum of diagonal and set value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string()- set the value of a pointer to a character
+ * @s: points to a pointer
+ * @to: points to a character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagsums- print the sum of two diagonals of a square matrix
+ * @a: 2d array of integers
+ * @size: size of an array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum, square;
+
+	i = 0;
+	sum = 0;
+	square  = size * size;
+	while (i < square)
+	{
+		if (i % (size + 1) == 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%d, ", sum);
+
+	while (i < square)
+	{
+		if (i % (size - 1) == 0 && i != (square - 1) && i != 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%d\n", sum);
+}
