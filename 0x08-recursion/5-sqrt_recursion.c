76b227ddd45a51db4162883d37a1022a14c7f9b6
hellenah254 <hellenah243@gmail.com>
2023-09-27 18:48:49 +0300

    return the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,8 +8,31 @@
 
 int _sqrt_recursion(int n)
 {
+	return (sqrt_recursion(n, 0));
+}
+
+/**
+  * sqrt_recursion()- recursive helper function
+  * @n: integer for which we want to find the square root
+  * @j: current guess for the square root
+  *
+  * Return: Always 0
+  */
+
+int sqrt_recursion(int n, int j)
+{
+	/* If n < 0, return -1*/
 	if (n < 0)
 		return (-1);
-	else
-		return (sqrt(n));
+
+	/* if square of j = n, return j*/
+	if ((j * j) == n)
+		return (j);
+
+	/*If square of j > n, then it's not a whole number*/
+	if ((j * j) > n)
+		return (-1);
+
+	/*Recursively increment the guess and continue searching*/
+	return (sqrt_recursion(n, j + 1));
 }
