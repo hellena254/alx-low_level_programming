7cc563088f1c4ac68fdee21d59f03c20e42bdd44
hellenah254 <hellenah243@gmail.com>
2023-10-10 11:37:10 +0300

    a simple struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure
+ * @name: firts element
+ * @age: second element
+ * @owner: third element
+ *
+ * Description: a simple struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
