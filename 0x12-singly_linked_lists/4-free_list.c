c2888ab79289047046a094bb593bf854a16b6b58
hellenah254 <hellenah243@gmail.com>
2023-10-20 18:50:57 +0300

    a function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: A pointer to the first node of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
