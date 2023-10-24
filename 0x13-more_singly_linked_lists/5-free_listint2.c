0564869509845ea0bd4f2a8c23b4eeaee9d0f996
hellenah254 <hellenah243@gmail.com>
2023-10-24 19:25:31 +0300

    edit number 5 and 6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		current = *head;
-		*head = current->next;
+		*head = (*head)->next;
 		free(current);
 	}
 }
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,13 @@ int pop_listint(listint_t **head)
 	int data = 0;
 	listint_t *temp;
 
-	if (head != NULL && *head != NULL)
-	{
+	if (*head == NULL)
+		return (0);
+
 		temp = *head;
 		data = temp->n;
 		*head = temp->next;
-	}
+		free(temp);
 
 	return (data);
 }
