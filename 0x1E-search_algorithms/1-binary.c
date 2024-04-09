76b6db9709628a5b86a7bd92c8e065601eb82412
hellena254 <hellenah243@gmail.com>
2024-04-09 05:54:13 -0700

    function that searches for value in a sorted array of integers using Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,51 @@
 #include<stdio.h>
 
 /**
+ * binary_search_recursive - searches for a value in a sorted array of
+ * integers using the BSA recursively
+ *
+ * @array: pointer to the first element of the array to search in
+ * @left: left index of the subarray
+ * @right: right index of the subarray
+ * @value: value to search for
+ *
+ * Return: index where value is located
+ *          if value is not present in array or if array is NULL
+ */
+
+int binary_search_recursive(int *array, int left, int right, int value)
+{
+	int mid, i;
+
+	if (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+
+			if (i < right)
+				printf(",");
+		}
+
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			return (binary_search_recursive(array, mid + 1, right, value));
+		else
+			return (binary_search_recursive(array, left, mid - 1, value));
+	}
+
+	return (-1);
+}
+
+
+
+/**
  * binary_search - searches for a value in a sorted array of
  *              integers using the BSA
  *
@@ -14,33 +59,8 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int left = 0, mid;
-	int right = size - 1;
-	size_t i;
-
 	if (array == NULL)
 		return (-1);
 
-	while (left <= right)
-	{
-		mid = (left + right) / 2;
-		printf("Searching in array: ");
-
-		for (i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i < right)
-				printf(",");
-		}
-		printf("\n");
-
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] < value)
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
-
-	return (-1);
+	return (binary_search_recursive(array, 0, size - 1, value));
 }
